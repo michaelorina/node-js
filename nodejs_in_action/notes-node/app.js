1514e52eba9e97f4c9717680b8d4f4e5d64da282
michaelorina <orinamichael4@gmail.com>
2024-09-24 10:38:53 +0300

    node_js_in_action third party modules

--- nodejs_in_action/notes-node/app.js
+++ nodejs_in_action/notes-node/app.js
@@ -2,10 +2,17 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash')
 const notes = require('./notes')
 
 // var res = notes.addNote();
 // console.log(res)
+
+console.log(_.isString(true))
+console.log(_.isString('Michael'))
+
+let filteredArray = _.uniq(['Michael', 1, 'Michael', 1, 2, 2, 3]);
+console.log(filteredArray)
 
 var res = notes.add(7, 3);
 console.log(res)
