30242f45cb1ca3fa973f201d7f26b1dddad8de69
michaelorina <orinamichael4@gmail.com>
2024-10-07 11:20:35 +0300

    node_js_in_action yargs

--- nodejs_in_action/notes-node/notes.js
+++ nodejs_in_action/notes-node/notes.js
@@ -2,12 +2,24 @@ console.log("Starting notes.js")
 
 // module.exports.age = 22;
 
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New note';
+var addNote = (title, body) => {
+    console.log('Adding note', title, body);
 }
 
-module.exports.add = (a, b) => {
-    console.log('Adding 2 numbers');
-    return a + b
+var getAll = () => {
+    console.log('Getting all notes');
+}
+
+var getNote = (title) => {
+    console.log('Getting note', title)
+}
+
+var removeNote = (title) => {
+    console.log('Removing Note', title)
+}
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
 }
--- nodejs_in_action/notes-node/userInput.js
+++ nodejs_in_action/notes-node/userInput.js
@@ -2,20 +2,22 @@ console.log('Starting userInput.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs')
 
-const notes = require('./notes')
+const notes = require('./notes');
+const argv = yargs.argv
 
 var command = process.argv[2];
 console.log('Command:', command)
 
 if (command === 'add') {
-    console.log('Adding new note');
+    notes.addNote(argv.title, argv.body)
 } else if (command === 'list') {
-    console.log('Listing all notes');
+    notes.getAll()
 } else if (command === 'read') {
-    console.log('Fetching note')
+    notes.getNote(argv.title)
 } else if (command === 'remove') {
-    console.log('Removing note')
+    notes.removeNote(argv.title)
 } else {
     console.log('Command not recognized');
 }
